e8f4fd1b283068fc4c681b3b2b63a36f9198fa4b
Keshav Sharma <keshavsharma1622@gmail.com>
2026-06-01 16:43:05 +0530

    commit

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import { Link } from 'expo-router'
+
+const SignUp = () => {
+  return (
+    <View>
+      <Text>signIn</Text>
+      <Link href="/">Sign Un</Link>
+    </View>
+  )
+}
+
+export default SignUp
